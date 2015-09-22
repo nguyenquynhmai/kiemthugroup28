74f6eeefc9f635831262f0d616c7f4aa022f5f49
Nguyen Quynh Mai <mainq_56@vnu.edu.vn>
2015-09-22 21:50:40 +0700

    updated

---
+++ lab01/script.js
@@ -0,0 +1,20 @@
+function isPrime(a) {		
+	if (a <= 0)		
+		return false;		
+	if ((a == 1) || (a==2)){		
+	  return true;		
+	} else {		
+		if ((a % 2) == 0) {		
+			return false;		
+		}else {		
+			var howFar = Math.abs(Math.sqrt(a));		
+	  		for(var icount = 3; icount <= howFar; icount = icount+2){		
+	    		if(a % icount == 0) {		
+	      			return false;		
+	    		}		
+	  		}		
+		}		
+	return true;  		
+	}		
+			
+}
